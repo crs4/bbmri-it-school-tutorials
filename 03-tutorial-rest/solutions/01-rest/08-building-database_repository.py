782520fbb15bd90a740fa93a907c8753bc1bb8e7
Vittorio Meloni <vittorio.meloni@crs4.it>
2025-07-15 18:09:53 +0200

    add post participants

---
+++ 03-tutorial-rest/solutions/01-rest/08-building-database_repository.py
@@ -0,0 +1,66 @@
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from biobank_manager.database.models import Participant, Sample, Diagnosis
+
+
+def get_all_participants(session: Session):
+    stmt = select(Participant)
+    return session.execute(stmt).scalars().all()
+
+
+def get_participant_by_id(session: Session, id: int):
+    stmt = select(Participant).where(Participant.id == id)
+    return session.execute(stmt).scalar()
+
+
+def create_participant_data(session, data: dict):
+    participant = Participant(**data)
+    session.add(participant)
+    session.commit()
+    session.refresh(participant)
+    return participant
+    
+
+
+def get_participant_id_by_first_name_and_last_name(
+    session: Session, first_name: str, last_name: str
+):
+    stmt = (
+        select(Participant.id)
+        .where(Participant.first_name == first_name)
+        .where(Participant.last_name == last_name)
+    )
+    return session.execute(stmt).scalar()
+
+
+def get_samples_for_participant_by_id(session: Session, id: int):
+    stmt = select(Sample).where(Sample.participant_id == id)
+    return session.execute(stmt).scalars().all()
+
+
+def get_samples_for_participant_by_first_name_and_last_name(
+    session: Session, first_name: str, last_name: str
+):
+    stmt = (
+        select(Sample)
+        .join(Participant)
+        .where(Participant.first_name == first_name)
+        .where(Participant.last_name == last_name)
+    )
+    return session.execute(stmt).scalars().all()
+
+
+def get_samples_by_type(session, sample_types):
+    stmt = select(Sample).where(Sample.type.in_(sample_types))
+    return session.execute(stmt).scalars().all()
+
+
+def get_diagnosis_for_participant_by_id(session, id):
+    stmt = select(Diagnosis).where(Diagnosis.participant_id == id)
+    return session.execute(stmt).scalars().all()
+
+
+def get_diagnosis_before_date(session, date):
+    stmt = select(Diagnosis).where(Diagnosis.date < date)
+    return session.execute(stmt).scalars().all()
---
+++ 03-tutorial-rest/solutions/01-rest/08-building-dtos_participants.py
@@ -0,0 +1,21 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel, Field
+
+class ParticipantCreateDTO(BaseModel):
+  prefix: Optional[str] = Field(title="An honorific prefix for the person (e.g., Dr.)")
+  suffix: Optional[str] = Field(title="An honorific suffix for the person (e.g., MD.)")
+  last_name: str = Field(title="Last name of the participant")
+  first_name: str = Field(title="First name of the participant")
+  date_of_birth: date = Field(title="Date of birth of the participant in ISO format (YYYY-MM-DD)")
+  place_of_birth: str = Field(title="Place of birth of the participant")
+  ssn: str = Field(title="Social Security Number of the participant")
+  gender: str = Field("Gender of the participant")
+
+class ParticipantReadDTO(ParticipantCreateDTO):
+  id: int = Field(title="Unique identifier for the participant")
+
+  @computed_field
+  @property
+  def name(self) -> str:
+    return f"{self.first_name} {self.last_name}"
